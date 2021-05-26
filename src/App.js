0ef03c04e6b1e5e5406f9875542d39babc10c5c8
markporo <markporo@yahoo.com>
2021-05-26 02:13:51 -0400

    getting components set up for Nasa API react project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import API_KEY from "./constants/APIKEY"
+import axios from 'axios';
+
 
 function App() {
+  const [NASAData, setNASAData] = useState([]);
+
+
   return (
     <div className="App">
+      <h1>NASA PHOTO OF THE DAY!</h1>
+      <img src="" alt="Sweet NASA Photo">
+      {/* {//date
+      //description} */}
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
---
+++ src/constants/APIKEY.js
@@ -0,0 +1,4 @@
+export const API_KEY = "KmCvdf1DTvWX6KAb3iNICzye4X7pevF0VEaEiEDs";
+
+// export const BASE_URL = 'http://localhost:4000'
+// export const API_KEY = 'xyz'
