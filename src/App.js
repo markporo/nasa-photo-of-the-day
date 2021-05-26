378681f89d37e1d7f3cc7d790b743df639b76b1c
markporo <markporo@yahoo.com>
2021-05-26 02:49:39 -0400

    get request getting Nasa api using axios is a success; App shogwing NASA photo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import API_KEY from "./constants/APIKEY"
+import { API_KEY } from "./constants/APIKEY"
+import NasaPhoto from "./components/NasaPhoto";
+import Home from "./components/Home";
 import axios from 'axios';
 
 
@@ -8,16 +10,35 @@ function App() {
   const [NASAData, setNASAData] = useState([]);
 
 
+  useEffect(() => {
+    // use Axios to fetch Nasa Data
+    // on success, use setNASAData to put in state 
+    // const apiUrl = BASE_URL + '/friends' + '?api_key=' + API_KEY
+    const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        setNASAData(res.data);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+
+
   return (
     <div className="App">
       <h1>NASA PHOTO OF THE DAY!</h1>
-      <img src="" alt="Sweet NASA Photo">
+      <img src={NASAData.url} alt="dope ass NASA PHOTO" />
       {/* {//date
       //description} */}
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+        {NASAData.date} <span role="img" aria-label='go!'>🚀</span>!
+      </p><h3>{NASAData.title}</h3>
+
+      <p>{NASAData.explanation}</p>
     </div>
   );
 }
